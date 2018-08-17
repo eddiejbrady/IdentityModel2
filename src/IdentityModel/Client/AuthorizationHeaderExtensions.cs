3b6442ddd8177b16cf3caa857a64f568936829b1
Dominick Baier <dbaier@leastprivilege.com>
2018-08-17 14:28:56 +0200

    Consolidate extensions for setting authZ header

--- src/IdentityModel/Client/AuthorizationHeaderExtensions.cs
+++ src/IdentityModel/Client/AuthorizationHeaderExtensions.cs
@@ -8,7 +8,7 @@ namespace System.Net.Http
     /// <summary>
     /// Extensions for HttpClient
     /// </summary>
-    public static class HttpClientExtensions
+    public static class AuthorizationHeaderExtensions
     {
         /// <summary>
         /// Sets a basic authentication header.
@@ -51,6 +51,49 @@ namespace System.Net.Http
         public static void SetBearerToken(this HttpClient client, string token)
         {
             client.SetToken("Bearer", token);
+        }
+
+        /// <summary>
+        /// Sets a basic authentication header.
+        /// </summary>
+        /// <param name="request">The HTTP request message.</param>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        public static void SetBasicAuthentication(this HttpRequestMessage request, string userName, string password)
+        {
+            request.Headers.Authorization = new BasicAuthenticationHeaderValue(userName, password);
+        }
+
+        /// <summary>
+        /// Sets a basic authentication header for RFC6749 client authentication.
+        /// </summary>
+        /// <param name="request">The HTTP request message.</param>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        public static void SetBasicAuthenticationOAuth(this HttpRequestMessage request, string userName, string password)
+        {
+            request.Headers.Authorization = new BasicAuthenticationOAuthHeaderValue(userName, password);
+        }
+
+        /// <summary>
+        /// Sets an authorization header with a given scheme and value.
+        /// </summary>
+        /// <param name="request">The HTTP request message.</param>
+        /// <param name="scheme">The scheme.</param>
+        /// <param name="token">The token.</param>
+        public static void SetToken(this HttpRequestMessage request, string scheme, string token)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue(scheme, token);
+        }
+
+        /// <summary>
+        /// Sets an authorization header with a bearer token.
+        /// </summary>
+        /// <param name="request">The HTTP request message.</param>
+        /// <param name="token">The token.</param>
+        public static void SetBearerToken(this HttpRequestMessage request, string token)
+        {
+            request.SetToken("Bearer", token);
         }
     }
 }
